271eb5eb036730579826db8e77c335dddf0faeb5
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-04-22 07:10:36 +0200

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,8 +1,8 @@
 export default async (req, res) => {
   if (req.method === 'POST') {
     console.log('Received webhook:', req.body);
-    res.status(200).json({ success: true });
-  } else {
-    res.status(405).send('Method Not Allowed');
+    return res.status(200).json({ success: true });
   }
-}
+  
+  return res.status(405).json({ error: 'Method not allowed' });
+};
