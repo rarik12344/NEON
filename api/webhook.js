f4f0d5efd7d9a6675434300530c508763885a187
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-04-27 00:46:19 +0200

    Delete api directory

--- api/webhook.js
+++
@@ -1,8 +0,0 @@
-export default async (req, res) => {
-  if (req.method === 'POST') {
-    console.log('Received webhook:', req.body);
-    return res.status(200).json({ success: true });
-  }
-  
-  return res.status(405).json({ error: 'Method not allowed' });
-};
