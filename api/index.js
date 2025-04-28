5674db0dcef82eda9deff37e06f9fd2ccd3384de
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-04-28 20:59:42 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 export default async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  
   return res.json({
     frame: {
       version: "vNext",
       image: "https://i.ibb.co/qMYFwLc6/hueon.jpg",
       imageAspectRatio: "1.91:1",
       buttons: [{
-        label: "🎫 Buy Tickets",
+        label: "🎫 Buy Tickets", 
         action: "post"
       }],
       postUrl: "https://neon-xi.vercel.app/api/frame"
