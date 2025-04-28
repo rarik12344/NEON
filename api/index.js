9022d2f9e81647aa275c94df496d4928d2c2afdd
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-04-28 20:56:22 +0200

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,16 @@
+export default async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  
+  return res.json({
+    frame: {
+      version: "vNext",
+      image: "https://i.ibb.co/qMYFwLc6/hueon.jpg",
+      imageAspectRatio: "1.91:1",
+      buttons: [{
+        label: "🎫 Buy Tickets",
+        action: "post"
+      }],
+      postUrl: "https://neon-xi.vercel.app/api/frame"
+    }
+  });
+}
