fcf59493b08b9557636df1d84fb61dbcb16ae836
rarik12344 <65827942+rarik12344@users.noreply.github.com>
2025-04-28 22:53:36 +0200

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,19 +1,15 @@
 export default async (req, res) => {
-  // Устанавливаем заголовки
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  const MINIAPP_URL = "https://neon-xi.vercel.app/?miniapp=true&source=frame"
   
-  // URL для редиректа в MiniApp
-  const redirectUrl = 'https://neon-xi.vercel.app/?miniapp=true&source=frame';
+  res.setHeader('Content-Type', 'application/json')
   
-  // Возвращаем ответ с редиректом
   return res.json({
     version: "vNext",
     status: "302",
-    location: redirectUrl,
+    location: MINIAPP_URL,
     meta: {
       "og:image": "https://i.ibb.co/HfcPqDfC/ogneon.jpg",
-      "og:title": "Neon Lottery - Купить билеты"
+      "og:title": "Neon Lottery | Daily ETH Draws"
     }
-  });
-};
+  })
+}
